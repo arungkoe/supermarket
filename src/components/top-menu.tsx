7cc583cca2bf0cd9a50583558f05be176ea0eda6
Awish Dot <awishdotid@gmail.com>
2024-08-18 21:04:46 +0800

    mulai ketik navbar menu top

--- src/components/top-menu.tsx
+++ src/components/top-menu.tsx
@@ -1,5 +1,27 @@
-
+import Link from "next/link";
 export default function TopMenu() {
   return (
+	<header className="shopee-top">
+		<div className="wraper navbar">
+			<nav className="container navbar">
+				<div className="navbar_center">
+					<Link href="">Seller Center</Link>
+					<Link href="">Mulai Berjualan</Link>
+				</div>	
+				<div className="navbar_space">
+				</div>	
+				<div className="navbar_links">
+					<ul>
+						<li>
+							<Link href="">Notifikasi</Link
+							<Link href="">Bantuan</Link>
+						</li>
+					</ul>
+				</div>	
+			</nav>
+		</div>
+		<div>
+		</div>
+	</header>
   )
 }
