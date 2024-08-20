ba7aa685a0db7360240b904a25724f1aaf044ef7
Awish Dot <awishdotid@gmail.com>
2024-08-20 23:05:52 +0800

    pengerjaan navbar top-menu kanan

--- src/components/top-menu.tsx
+++ src/components/top-menu.tsx
@@ -1,26 +1,32 @@
 import Link from "next/link";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaUniversity } from "react-icons/fa";
+
 export default function TopMenu() {
   return (
-	<header className="shopee-top">
-		<div className="wraper navbar">
-			<nav className="container navbar bg-blue-600 text-white text-sm flex justify-between">
-				<div className="navbar_center font-light">
-					<Link href="#">Seller Center</Link>
-					<Link href="#">Mulai Berjualan</Link>
-					<div><Link href="#">Download</Link></div>
-					<div><Link href="#">Ikuti Kami</Link></div>
-					<div>
-						<Link href="#">Fb</Link>
-						<Link href="#">Ig</Link>
-						<Link href="#">Km</Link>
+	<header className="shopee-top-menu mx-auto p-2">
+		<div className="wraper navbar ">
+			<nav className="container navbar bg-blue-600 text-white text-sm flex justify-between p-2">
+				<div className="navbar_center font-light flex">
+					<Link href="#" className="block px-1 after:content-[''] after:opacity-50 after:ml-2 after:border-l after:-left-[6px] after:-inset-4 after:text-xs">Seller Center</Link>
+					<Link href="#" className="block px-1 after:content-[''] after:opacity-50 after:ml-2 after:border-l after:-left-[6px] after:-inset-4 after:text-xs">Mulai Berjualan</Link>
+					<Link href="#" className="block px-1 after:content-[''] after:opacity-50 after:ml-2 after:border-l after:-left-[6px] after:-inset-4 after:text-xs">Download</Link>
+					<Link href="#" className="block px-1">Ikuti Kami</Link>
+					<div className="block flex gap-2 py-1">
+						<Link href="#"><FaFacebook /></Link>
+						<Link href="#"><FaInstagram /></Link>
+						<Link href="#"><FaUniversity /></Link>
 					</div>
 				</div>	
-				<div className="navbar_space">
+				<div className="navbar_space bg-white h-2 block">
 				</div>	
-				<div className="navbar_links">
-					<ul>
-						<li>
+				<div className="navbar_links flex">
+					<ul className="block flex w-full">
+						<li className="block px-4">
 							<Link href="">Notifikasi</Link>
+						</li>
+						<li className="block px-4">
 							<Link href="">Bantuan</Link>
 						</li>
 					</ul>
