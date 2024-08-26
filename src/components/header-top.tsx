5dc999dfacd2a8ee0ec1eb8de819748375c8d2db
Awish Dot <awishdotid@gmail.com>
2024-08-26 22:32:32 +0800

    buat sketsa header 1

---
+++ src/components/header-top.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link"; 
+
+export default function HeaderTop() {
+  return (
+		<div className="header-wrapper">
+			<div className="header-top">
+				<div className="">Brand</div>
+			</div>
+			<div className="header-space">
+			
+			</div>
+			<nav className="header-top">
+				<p>Menu Navigation</p>
+			</nav>
+		</div>
+	
+  )
+}
