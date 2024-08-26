c019b72fc2a3a7702030b51cdf09b1d001627ea1
Awish Dot <awishdotid@gmail.com>
2024-08-26 14:29:32 +0800

    ganti supermarket

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import TopMenu from "@/components/top-menu";
+import HeaderContent from "@/components/header-content";
 
 export default function Home() {
   return (
   <>
-	<TopMenu />
+	<header className="shopee-top-menu mx-auto p-2 bg-blue-600 w-full ">
+		<TopMenu />
+		<HeaderContent />
+	</header>
  
 	</>
   );
